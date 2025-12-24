7da6b430b94729b1fbb69654a78745821444a959
allan716 <525223688@qq.com>
2025-12-24 18:04:02 +0800

    simplify(setup-certs): use single client certificate for all services
    - All client services now use the same client.crt/client.key
    - Simpler deployment - just copy client/ folder to any service

--- deploy/nats/tls/setup-certs.bat
+++ deploy/nats/tls/setup-certs.bat
@@ -10,7 +10,7 @@ echo ========================================
 echo.
 echo This will create:
 echo   1. nats-server/ folder - for NATS server
-echo   2. client/ folder - for client services
+echo   2. client/ folder - for all client services
 echo.
 
 REM Get script directory
@@ -21,7 +21,7 @@ REM ========================================
 REM Find OpenSSL
 REM ========================================
 
-echo [1/7] Locating OpenSSL...
+echo [1/6] Locating OpenSSL...
 set "OPENSSL_CMD="
 
 where openssl >nul 2>&1
@@ -56,7 +56,7 @@ REM ========================================
 REM Generate CA Certificate
 REM ========================================
 
-echo [2/7] Generating CA certificate...
+echo [2/6] Generating CA certificate...
 "%OPENSSL_CMD%" genrsa -out ca.key 2048
 if errorlevel 1 goto error
 "%OPENSSL_CMD%" req -new -x509 -days 10950 -key ca.key -out ca.crt -subj "/CN=LightLink CA"
@@ -68,7 +68,7 @@ REM ========================================
 REM Generate NATS Server Certificate
 REM ========================================
 
-echo [3/7] Generating NATS server certificate...
+echo [3/6] Generating NATS server certificate...
 "%OPENSSL_CMD%" genrsa -out server.key 2048
 if errorlevel 1 goto error
 "%OPENSSL_CMD%" req -new -key server.key -out server.csr -subj "/CN=nats-server"
@@ -79,31 +79,24 @@ echo NATS server certificate created
 echo.
 
 REM ========================================
-REM Generate Client Certificates
+REM Generate Client Certificate
 REM ========================================
 
-echo [4/7] Generating client certificates...
-
-"%OPENSSL_CMD%" genrsa -out demo-service.key 2048
-"%OPENSSL_CMD%" req -new -key demo-service.key -out demo-service.csr -subj "/CN=demo-service"
-"%OPENSSL_CMD%" x509 -req -days 10950 -in demo-service.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out demo-service.crt
-
-"%OPENSSL_CMD%" genrsa -out test-service.key 2048
-"%OPENSSL_CMD%" req -new -key test-service.key -out test-service.csr -subj "/CN=test-service"
-"%OPENSSL_CMD%" x509 -req -days 10950 -in test-service.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out test-service.crt
-
-"%OPENSSL_CMD%" genrsa -out client-app.key 2048
-"%OPENSSL_CMD%" req -new -key client-app.key -out client-app.csr -subj "/CN=client-app"
-"%OPENSSL_CMD%" x509 -req -days 10950 -in client-app.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out client-app.crt
-
-echo Client certificates created
+echo [4/6] Generating client certificate (for all clients)...
+"%OPENSSL_CMD%" genrsa -out client.key 2048
+if errorlevel 1 goto error
+"%OPENSSL_CMD%" req -new -key client.key -out client.csr -subj "/CN=lightlink-client"
+if errorlevel 1 goto error
+"%OPENSSL_CMD%" x509 -req -days 10950 -in client.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out client.crt
+if errorlevel 1 goto error
+echo Client certificate created
 echo.
 
 REM ========================================
 REM Create NATS Server Folder
 REM ========================================
 
-echo [5/7] Creating nats-server folder...
+echo [5/6] Creating nats-server folder...
 if exist "nats-server" rmdir /S /Q nats-server
 mkdir nats-server
 
@@ -118,17 +111,13 @@ REM ========================================
 REM Create Client Folder
 REM ========================================
 
-echo [6/7] Creating client folder...
+echo [6/6] Creating client folder...
 if exist "client" rmdir /S /Q client
 mkdir client
 
 copy /Y ca.crt client\ >nul
-copy /Y demo-service.crt client\ >nul
-copy /Y demo-service.key client\ >nul
-copy /Y test-service.crt client\ >nul
-copy /Y test-service.key client\ >nul
-copy /Y client-app.crt client\ >nul
-copy /Y client-app.key client\ >nul
+copy /Y client.crt client\ >nul
+copy /Y client.key client\ >nul
 
 echo client/ folder created
 echo.
@@ -137,7 +126,7 @@ REM ========================================
 REM Generate README Files
 REM ========================================
 
-echo [7/7] Generating documentation...
+echo Generating documentation...
 
 REM README for nats-server folder
 echo # NATS Server Certificate Package > nats-server\README.txt
@@ -147,24 +136,26 @@ echo. >> nats-server\README.txt
 echo Files: >> nats-server\README.txt
 echo   - ca.crt (CA certificate) >> nats-server\README.txt
 echo   - server.crt (Server certificate) >> nats-server\README.txt
-echo   - server.key (Server private key) >> nats-server\README.txt
+echo   - server.key (Server private key - KEEP SECRET!) >> nats-server\README.txt
 
 REM README for client folder
 echo # Client Certificate Package > client\README.txt
 echo. >> client\README.txt
 echo Copy this entire folder to your client service directory. >> client\README.txt
+echo All client services can use the same certificates. >> client\README.txt
 echo. >> client\README.txt
 echo Files: >> client\README.txt
 echo   - ca.crt (CA certificate) >> client\README.txt
-echo   - demo-service.crt / demo-service.key >> client\README.txt
-echo   - test-service.crt / test-service.key >> client\README.txt
-echo   - client-app.crt / client-app.key >> client\README.txt
+echo   - client.crt (Client certificate - for ALL services) >> client\README.txt
+echo   - client.key (Client private key - KEEP SECRET!) >> client\README.txt
 echo. >> client\README.txt
 echo Configuration example: >> client\README.txt
 echo   ca: client/ca.crt >> client\README.txt
-echo   cert: client/demo-service.crt >> client\README.txt
-echo   key: client/demo-service.key >> client\README.txt
+echo   cert: client/client.crt >> client\README.txt
+echo   key: client/client.key >> client\README.txt
 echo   server_name: nats-server >> client\README.txt
+echo. >> client\README.txt
+echo NOTE: This single client certificate can be used by ALL services. >> client\README.txt
 
 echo Documentation created
 echo.
@@ -191,14 +182,13 @@ echo     - ca.crt
 echo     - server.crt
 echo     - server.key
 echo.
-echo [2] client/ - For client services
+echo [2] client/ - For ALL client services
 echo     - ca.crt
-echo     - demo-service.crt / demo-service.key
-echo     - test-service.crt / test-service.key
-echo     - client-app.crt / client-app.key
+echo     - client.crt (same for all clients)
+echo     - client.key (same for all clients)
 echo.
 echo Deploy nats-server/ to your NATS server.
-echo Deploy client/ to your client services.
+echo Deploy client/ to ANY client service (all use same certificate).
 echo.
 pause
 goto end
