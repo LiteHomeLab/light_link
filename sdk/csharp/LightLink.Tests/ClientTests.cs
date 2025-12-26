d4067464d5b4e94ff652d415dbea425440d08b67
allan716 <525223688@qq.com>
2025-12-26 17:35:32 +0800

    test(csharp): add empty ClientTests class

---
+++ sdk/csharp/LightLink.Tests/ClientTests.cs
@@ -0,0 +1,16 @@
+using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LightLink.Tests
+{
+    public class ClientTests : IDisposable
+    {
+        // Tests will be added in subsequent tasks
+        public void Dispose()
+        {
+            // Cleanup code
+        }
+    }
+}
